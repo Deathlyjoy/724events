7e4ead934e005078e8b57943b0f50333ce988093
Deathlyjoy <colombelle.quentin@icloud.com>
2023-09-26 16:14:34 +0200

    Ancrage ID pour navbar
    Ajout des id pour ancrer avec la navbar de l'header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,8 +22,8 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
-        <h2 className="Title">Nos services</h2>
+      <section className="ServicesContainer" id="nos-services"> {/* Ajout id -> ancrage nav header */}
+        <h2 className="Title">Nos services</h2> 
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
           <ServiceCard imageSrc="/images/priscilla-du-preez-Q7wGvnbuwj0-unsplash1.png">
@@ -51,12 +51,12 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
-        <h2 className="Title">Nos réalisations</h2>
+      <section className="EventsContainer" id="nos-realisations"> {/* Ajout id -> ancrage nav header */}
+        <h2 className="Title">Nos réalisations</h2> 
         <EventList />
       </section>
-      <section className="PeoplesContainer">
-        <h2 className="Title">Notre équipe</h2>
+      <section className="PeoplesContainer" id="notre-equipe"> {/* Ajout id -> ancrage nav header */}
+        <h2 className="Title">Notre équipe</h2> 
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
           <PeopleCard
