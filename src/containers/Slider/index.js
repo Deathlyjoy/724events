828f9f2c8039aef61c67cf79555ed4a5c0abc913
Deathlyjoy <colombelle.quentin@icloud.com>
2023-09-26 17:12:56 +0200

    Debug sens défilement slider + suppression page blanche
    Le slider affiche les évènements en décroissant et la dernière page blanche a disparu

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,12 +8,12 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1 //Changement du signe < en > afin d'effectuer un défilement des évènements décroissant
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
+      // Suppression de la page blanche du slider : ajout du -1 à byDateDesc.length afin index + 1 ne dépasse pas la longueur du tableau
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), 5000
     );
   };
   useEffect(() => {
