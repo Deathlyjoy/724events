8dc91f046ee61a5ad91d3cd3615cba397dc6f104
Deathlyjoy <colombelle.quentin@icloud.com>
2023-09-27 04:16:50 +0200

    Correction Tri Catégorie Toutes
    Modif de Events + Select pour résoudre le bug du tri des réalisations

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,7 +13,8 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = ((!type? data?.events : data?.events) || []).filter(
+  const filteredEvents = ((!type ? data?.events : data?.events.filter((e) => e.type === type)) || []).filter(
+  //const filteredEvents = ((!type? data?.events : data?.events) || []).filter(
     (event, index) => {
       if (
         (currentPage - 1) * PER_PAGE <= index &&
