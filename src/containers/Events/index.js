7bc1ef334819b7a51441a5612a6b0f88142e67f3
Deathlyjoy <colombelle.quentin@icloud.com>
2023-10-04 05:50:53 +0200

    back up bis

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -14,6 +14,7 @@ const EventList = () => {
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = ((!type ? data?.events : data?.events.filter((e) => e.type === type)) || []).filter(
+    
   //const filteredEvents = ((!type? data?.events : data?.events) || []).filter(
     (event, index) => {
       if (
